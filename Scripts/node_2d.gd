5a4f3fec96cb690cd15b4ee034d508aa2cf301d0
Dwelwin <157803725+Dwelwin@users.noreply.github.com>
2025-01-25 14:38:47 +0200

    Added music

--- Scripts/node_2d.gd
+++ Scripts/node_2d.gd
@@ -3,21 +3,21 @@ extends Node2D
 @onready var map2 = $Hand/Hand_sprite
 @onready var map3 = $FloatingTausta
 var change = 1.0
-var rise = true
+var rise = false
 
 func _ready() -> void:
 	pass
 	
 func _process(delta: float) -> void:
-	if change <= -10.0:
-		rise = true
-	if change >= 2:
-		rise = false
+	#if change <= -10.0:
+	#	rise = true
+	#if change >= 2:
+	#	rise = false
 	
 	if rise:
-		change += 0.1
+		change += 0.05
 	else:
-		change -= 0.1
+		change -= 0.01
 	
 	map1.material.set("shader_parameter/contrast", change)
 	map2.material.set("shader_parameter/contrast", change)
